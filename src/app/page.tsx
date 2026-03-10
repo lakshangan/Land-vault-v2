328070353b1ae9b7681c284676df6f2e1d9dbb80
Lakshan Ganesan <lakapple05@gmail.com>
2026-03-10 23:45:06 +0530

    Restore: Vision section and Upgrade: Hero with 3D technical visuals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Preloader from "@/components/Preloader";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import Problem from "@/components/Problem";
+import Vision from "@/components/Vision";
 import Solution from "@/components/Solution";
 import Ecosystem from "@/components/Ecosystem";
 import HowItWorks from "@/components/HowItWorks";
@@ -23,7 +23,7 @@ export default function Home() {
 
       <div style={{ position: 'relative', zIndex: 1 }}>
         <Hero />
-        <div id="protocol"><Problem /></div>
+        <div id="protocol"><Vision /></div>
         <Solution />
         <Ecosystem />
         <HowItWorks />
